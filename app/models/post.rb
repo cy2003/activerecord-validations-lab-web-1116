8cff7fd4a402fd36b8a6334311d36d8c1a246408
cy2003 <catyyee@hotmail.com>
2017-01-01 11:40:44 -0500

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: {minimum: 50}
+  validates :summary, length: {maximum: 50}
+  validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+  validates :title, format: {with: /Won't Believe|Secret|Top|Guess/}
 end
